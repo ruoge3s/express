ccbda7a6c123c6ca56186c89eb7107ad09060be7
ruoge3s <ruoge3s@gmail.com>
2019-05-16 17:00:30 +0800

    中通快递开放接口对接

--- demo/zto.php
+++ demo/zto.php
@@ -3,6 +3,8 @@
 include __DIR__ . '/../vendor/autoload.php';
 
 
+// 中通快递开放平台 https://zop.zto.com/apiDoc/
+
 $trace = new \ruoge3s\express\zto\Traces([
     'companyId'     => '中通授权的公司ID',
     'key'           => '中通授权的秘钥key'
